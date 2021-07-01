22e508295f83a1d237f80dd0789200581c76a17d
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2021-06-30 23:16:58 -0400

    Update accept buyout.R

--- accept buyout.R
+++ accept buyout.R
@@ -1,16 +1,29 @@
-starting_br = as.integer(readline(prompt = "Bankroll: "))
-offer = as.integer(readline(prompt = "Sportsbook Offer: "))
-init_bet = as.integer(readline(prompt = "Initial Bet: "))
-profit = offer - init_bet
-br_growth = profit / starting_br
+library(tidyverse)
+library(shiny)
 
-odds = as.integer(readline(prompt = "What are the original fractional odds?: "))
-estimated_prob = readline(prompt = "What is your estimated probability?: ")
-estimated_prob = as.numeric(estimated_prob)
-estimated_loss = 1 - estimated_prob
-fn = function(x){estimated_loss*log(1-x) + estimated_prob*log(1+15*x)}
-opt = optimize(fn, interval = c(0,1), maximum = 1)
-f = fn(opt$maximum)
-est_br_growth = exp(f) 
+app = shinyApp(ui = fluidPage(title = "Sportsbook Buyout Comparison",
+                              
+                              # inputs
+                              numericInput("br", "Bankroll", value = 1),
+                              numericInput("offer", "Offer from sportsbook", value = 1),
+                              numericInput("ib", "Initial Bet", value = 1),
+                              numericInput("odds", "American Odds of Initial Bet", value = 1),
+                              numericInput("estprob", "Estimated Probability", value = 0.5, min = 0, max = 1),
+                              
+                              # return
+                              h4("Guarateed Bankroll Growth: ", textOutput("sports_book_br_growth")),
+                              h4("Kelly Estimated Bankroll Growth: ", textOutput("kelly_br_growth"))),
+               server = function(input, output){
+                 output$sports_book_br_growth = renderText({
+                   round((input$offer - input$ib)/input$br + 1,4)
+                 })
+                 output$kelly_br_growth = renderText({
+                   convert_amer_odds = if_else(input$odds > 0, input$odds / 100, -100 / input$odds)
+                   estimated_loss = 1 - input$estprob
+                   br_size = ((convert_amer_odds)*input$estprob - estimated_loss)/(convert_amer_odds)
+                   round(exp(estimated_loss*log(1-br_size) + input$estprob*log(1+(convert_amer_odds-1)*br_size)),4)
+                 })
+               })
+runApp(app)                  
 
 
